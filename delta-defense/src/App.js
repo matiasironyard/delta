1100bad0dbf6198fdd97f22276cab664be1bbc23
matiasironyard <hi@matiasmariani.io>
2020-05-25 21:27:45 -0400

    add dependencies

--- delta-defense/src/App.js
+++ delta-defense/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import { ContextProvider } from "./components/synergy/synergy";
 
-function App() {
+import Layout from "./components/layout/index";
+
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <ContextProvider>
+        <Layout>
+          <Switch>
+            <Route exact path="/posts/">
+              <h1>Posts</h1>
+            </Route>
+            <Route exact path="/posts/:id">
+              <h1>Post details</h1>
+            </Route>
+            <Route exact path="/users/">
+              <h1>Users</h1>
+            </Route>
+            <Route exact path="/users/:id">
+              <h1>User Details</h1>
+            </Route>
+            <Redirect from="/" to="posts" />
+          </Switch>
+        </Layout>
+      </ContextProvider>
+    </BrowserRouter>
   );
 }
-
-export default App;
